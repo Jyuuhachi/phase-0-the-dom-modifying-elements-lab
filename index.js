c1ec56123ec30a81fd13cc8786de38c8df97c10a
Christopher <qaetre@gmail.com>
2023-05-23 13:47:49 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+//const deletion = document.getElementById("main")
+document.getElementById("main").remove()
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.textContent = `python is the champion`
+document.body.append(newHeader)
